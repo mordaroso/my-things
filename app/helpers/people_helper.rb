3ed5f1e195438da969097c1e5618c1eef494cf09
Fabio Kuhn <mordaroso@fbo-mbp.(none)>
2009-12-14 08:58:17 +0100

    use length instead of count (older ruby versions)

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,5 +1,5 @@
 module PeopleHelper
   def lender_link(lender, title = lender.name)
-    "#{title} (#{lender.borrowed_things.count })"
+    "#{title} (#{lender.borrowed_things.length })"
   end
 end
--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -1,6 +1,6 @@
 module ThingsHelper
   def tag_link(tag)
-    number = Thing.find_tagged_with(tag, :on => :tags).count
+    number = Thing.find_tagged_with(tag, :on => :tags).length
     "#{tag.name} (#{number})"
   end
 end
